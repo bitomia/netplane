d4bbecf86772c7f7db25b5459d04201fd763f30c
Juan Fernandez <juanfj@fastmail.com>
2025-01-13 15:52:30 +0100

    refactor: remove unnecesary code

--- src/tundev.rs
+++ src/tundev.rs
@@ -1,9 +1,5 @@
-use libc::{setsockopt, IPPROTO_IP, IP_HDRINCL};
 use log::info;
-use nix::sys::socket::{socket, AddressFamily, SockFlag, SockProtocol, SockType};
 use std::io::Read;
-use std::mem;
-use std::os::fd::AsRawFd;
 use tun::Configuration;
 
 pub struct TunDev {
@@ -14,27 +10,6 @@ impl TunDev {
     #[allow(unused_variables)]
     pub fn new(tun_name: String, ip_addr: String) -> Self {
         info!("TUN initialized for {}", ip_addr);
-
-        let raw_socket = socket(
-            AddressFamily::Inet,
-            SockType::Raw,
-            SockFlag::empty(),
-            SockProtocol::Raw,
-        )
-        .expect("Failed to create raw socket");
-        let option_value: libc::c_int = 1; // Enable IP_HDRINCL (1 = true)
-        let ret = unsafe {
-            setsockopt(
-                raw_socket.as_raw_fd(), // Socket descriptor
-                IPPROTO_IP,             // Protocol level
-                IP_HDRINCL,             // Option name
-                &option_value as *const _ as *const libc::c_void,
-                mem::size_of_val(&option_value) as libc::socklen_t,
-            )
-        };
-        if ret == -1 {
-            std::process::exit(1)
-        }
 
         let mut config = Configuration::default();
         config
