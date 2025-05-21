dd3f7efeadcdfbd6b0cee5c7942191f643029123
Juan Fernández <juan@bitomica.com>
2025-05-21 20:39:18 +0200

    wip

--- src/crypto.rs
+++ src/crypto.rs
@@ -4,6 +4,8 @@ use std::path::Path;
 use base64::{engine::general_purpose, Engine};
 use ed25519_dalek::{ed25519::signature::SignerMut, SigningKey, VerifyingKey, PUBLIC_KEY_LENGTH, SECRET_KEY_LENGTH};
 use rand::rngs::OsRng;
+use base64::{Engine as _, alphabet, engine::{self, general_purpose}};
+use ed25519_dalek::{SigningKey, SECRET_KEY_LENGTH, PUBLIC_KEY_LENGTH};
 use hmac::{Hmac, Mac}; // Mac is a trait, Hmac is the struct
 use sha2::Sha256;
 
@@ -36,6 +38,64 @@ pub fn try_load_auth_keys() -> Result<(SigningKey, VerifyingKey)> {
     let public = VerifyingKey::from_bytes(&<[u8; PUBLIC_KEY_LENGTH]>::try_from(public_key.as_slice())?)?;
     
     Ok((private, public))
+}
+
+
+fn snow_test() -> Result<(), anyhow::Error>
+{
+    let signing_key= "VJC7IabdEbYQvb88ulpjx6VrtCTYuuT+t6ww6/31XkA=";
+    let signing_key = general_purpose::STANDARD.decode(signing_key).unwrap();
+    let signing_key: [u8;32] = signing_key.try_into().unwrap();
+    let signing_key: SigningKey = SigningKey::from_bytes(&signing_key);
+    
+    static PATTERN: &'static str = "Noise_IK_25519_ChaChaPoly_BLAKE2s";
+    let initiator_keypair = snow::Builder::new(PATTERN.parse()?)
+        .generate_keypair()?;
+
+    let responder_keypair = snow::Builder::new(PATTERN.parse()?)
+        .generate_keypair()?;
+
+    // ====
+    
+    let mut initiator = snow::Builder::new(PATTERN.parse()?)
+        .local_private_key(&initiator_keypair.private)
+        .remote_public_key(&responder_keypair.public)
+        .build_initiator()?;
+    
+    let mut responder = snow::Builder::new(PATTERN.parse()?)
+        .local_private_key(&responder_keypair.private)
+        .remote_public_key(&initiator_keypair.public)
+        .build_responder()?;
+
+    let (mut read_buf, mut first_msg, mut second_msg) =
+        ([0u8; 1024], [0u8; 1024], [0u8; 1024]);
+
+    let mut buf = [0u8; 1024];
+    
+    // -> e
+    let len = initiator.write_message(&[], &mut first_msg)?;
+    
+    // responder processes the first message...
+    responder.read_message(&first_msg[..len], &mut read_buf)?;
+
+    // <- e, ee
+    let len = responder.write_message(&[], &mut second_msg)?;
+
+    // initiator processes the response...
+    initiator.read_message(&second_msg[..len], &mut read_buf)?;
+
+    // NN handshake complete, transition into transport mode.
+    let mut initiator = initiator.into_transport_mode()?;
+    let mut responder = responder.into_transport_mode()?;
+
+    let len = initiator.write_message(b"test\0", &mut buf).unwrap();
+    println!("{}", len);
+    
+    let mut read_buf = [0u8; 1024];
+    responder.read_message(&buf[..len], &mut read_buf).unwrap();
+    println!("client said: {}", String::from_utf8_lossy(&read_buf[..len]));
+
+    Ok(())
 }
 
 pub fn sign_key(key: String) -> Vec<u8> {
