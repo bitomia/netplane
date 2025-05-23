8156f776191313c7a4d6fe418f11187b38825124
Juan Fernández <juan@bitomica.com>
2025-05-23 09:01:53 +0200

    wip

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,8 @@
-use log::info;
-use sqlx::{sqlite::SqlitePoolOptions, Pool, Sqlite};
+use anyhow::anyhow;
+use sqlx::{sqlite::SqlitePoolOptions, Pool, Sqlite, FromRow};
 use std::{env, path::Path as FilePath};
+use serde::{Deserialize, Serialize};
+use log::info;
 
 pub enum ErrorCode {
     CannotConnect,
@@ -8,6 +10,19 @@ pub enum ErrorCode {
 
 pub struct Db {
     pub pool: Pool<Sqlite>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, FromRow)]
+pub struct Client {
+    pub id: String,
+    pub sdn_client_ip: String,
+    pub network: String,
+    pub netmask: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, FromRow)]
+pub struct AuthClient {
+    pub client_id: Option<String>,
 }
 
 impl Db {
@@ -32,9 +47,66 @@ impl Db {
     }
 
     pub async fn get_client_sdn_ip(self: &Self, client_key: &str) -> Option<String> {
-        sqlx::query_scalar("SELECT sdn_client_ip FROM clients WHERE client_key = $1")
+        sqlx::query_scalar("SELECT sdn_client_ip FROM clients WHERE public_key = $1")
             .bind(client_key)
             .fetch_optional(&self.pool)
             .await.ok()?
+    }
+
+    pub async fn create_client(self: &Self, client: &Client) -> Result<(), anyhow::Error> {
+        let mut tx = self.pool.begin().await?;
+        
+        sqlx::query("INSERT INTO clients (id, sdn_client_ip, network, netmask) VALUES (?, ?, ?, ?)")
+            .bind(&client.id)
+            .bind(&client.sdn_client_ip)
+            .bind(&client.network)
+            .bind(&client.netmask)
+            .execute(&mut *tx)
+            .await?;
+
+        let auth_link_id = uuid::Uuid::new_v4();
+        sqlx::query("INSERT INTO auth_links (id, client_id) VALUES (?, ?)")
+            .bind(&auth_link_id)
+            .bind(&client.id)
+            .execute(&mut *tx)
+            .await?;
+        
+        tx.commit().await?;
+        Ok(())
+    }
+
+    pub async fn is_auth(self: &Self, auth_id: &String) -> Result<AuthClient, anyhow::Error> {
+        let auth_entries = sqlx::query_as!(AuthClient, "SELECT client_id FROM auth_links WHERE id=? AND used=false LIMIT 1", auth_id)
+            .fetch_one(&self.pool)
+            .await?;
+        Ok(auth_entries)
+    }
+    
+    pub async fn auth_client(self: &Self, auth_id: &String, pub_key: &String) -> Result<(), anyhow::Error> {
+        let is_authed = self.is_auth(&auth_id).await?;
+        if is_authed.client_id.is_none() { return Err(anyhow!("Not authed")); }
+
+        let tx = self.pool.begin().await?;
+
+        // TODO
+        
+        tx.commit().await?;
+        Ok(())
+    }
+    
+    pub async fn get_all_clients(self: &Self) -> Result<Vec<Client>, anyhow::Error> {
+        let clients = sqlx::query_as!(Client, "SELECT id, sdn_client_ip, network, netmask FROM clients",)
+            .fetch_all(&self.pool)
+            .await?;
+        Ok(clients)
+    }
+    
+    pub async fn delete_client(self: &Self, client_id: &String) -> Result<(), anyhow::Error> {
+        sqlx::query("DELETE FROM clients WHERE id=? RETURNING *")
+            .bind(&client_id)
+            .execute(&self.pool)
+            .await?;
+
+        Ok(())
     }
 }
--- src/migrations/20250217_init.sql
+++ src/migrations/20250217_init.sql
@@ -1,7 +1,14 @@
 CREATE TABLE IF NOT EXISTS clients (
     id TEXT PRIMARY KEY NOT NULL,
-    client_key TEXT NOT NULL UNIQUE,
+    pub_key TEXT,
     sdn_client_ip TEXT NOT NULL UNIQUE,
     network TEXT NOT NULL,
     netmask TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS auth_links (
+    id TEXT PRIMARY KEY NOT NULL,
+    client_id TEXT,
+    used BOOLEAN DEFAULT false,
+    FOREIGN KEY (client_id) REFERENCES clients(id)
 );
