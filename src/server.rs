8e916663e9cf4aeefdee56f16259675858514bd7
Juan Fernández <juan@bitomica.com>
2025-06-09 18:11:05 +0200

    refactor: migration as server option

--- src/server.rs
+++ src/server.rs
@@ -2,11 +2,13 @@ use anyhow::Result;
 use common::{HandshakeRep, HandshakeReq, HandshakeStatus};
 use dotenv::dotenv;
 use log::{debug, error, info};
+use sqlx::sqlite::SqlitePoolOptions;
 use std::collections::{HashMap, HashSet};
 use std::net::{Ipv4Addr, SocketAddr};
+use std::path::Path as FilePath;
 use std::str::FromStr;
 use std::sync::Arc;
-use tokio::signal::unix::{SignalKind, signal};
+use tokio::signal::unix::{signal, SignalKind};
 
 pub mod common;
 pub mod crypto;
@@ -53,9 +55,7 @@ async fn reticula_server(db: Arc<db::Db>) -> Result<()> {
                         if src != client.src {
                             // TODO this is broadcasting, parse IP header and send only to target
                             debug!("...relying");
-                            socket
-                                .send_to(&buf[..amt], &client.src).await?;
-
+                            socket.send_to(&buf[..amt], &client.src).await?;
                         }
                     }
                 } else {
@@ -85,8 +85,9 @@ async fn reticula_server(db: Arc<db::Db>) -> Result<()> {
                                     );
                                     match socket.send_to(&reply.serialize()?, &src).await {
                                         Ok(_) => {
-                                            clients_status.insert(src, HandshakeStatus::Initialized);
-                                        },
+                                            clients_status
+                                                .insert(src, HandshakeStatus::Initialized);
+                                        }
                                         Err(_) => {
                                             // TODO
                                         }
@@ -109,6 +110,29 @@ async fn reticula_server(db: Arc<db::Db>) -> Result<()> {
     }
 }
 
+async fn do_migrate() {
+    let db_file_path = std::env::var("DATABASE_URL").unwrap();
+    let db_path = db_file_path.replace("sqlite://", "");
+
+    if !FilePath::new(&db_path).exists() {
+        info!("Database file not found, creating...");
+        std::fs::File::create(&db_path).expect("Failed to create SQLite file");
+    }
+    let pool = SqlitePoolOptions::new()
+        .connect(&db_file_path)
+        .await
+        .expect("Cannot connect to database");
+    sqlx::migrate!("./src/migrations")
+        .run(&pool)
+        .await
+        .expect("Migration failed");
+    println!("Migration successfully finished");
+}
+
+fn echo_syntax(args: &Vec<String>) {
+    println!("Use {} [--migrate]", args[0]);
+}
+
 #[tokio::main]
 async fn main() -> Result<(), ProcessError> {
     let mut sigterm = signal(SignalKind::terminate()).expect("Failed to bind SIGTERM handler");
@@ -127,6 +151,17 @@ async fn main() -> Result<(), ProcessError> {
     env_logger::init();
     dotenv().ok();
     std::env::var("SECRET_KEY").expect("SECRET_KEY env var not found");
+
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() == 2 {
+        if args[1] == "--migrate" {
+            do_migrate().await;
+            return Ok(());
+        } else {
+            echo_syntax(&args);
+            std::process::exit(1);
+        }
+    }
 
     let db = Arc::new(db::Db::new().await);
     let web_server = WebServer::new(db.clone()).await;
