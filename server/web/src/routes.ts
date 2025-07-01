191b44bc56bac8555f2fe669e8f7cb42a2af888b
Juan Fernández <juan@bitomica.com>
2025-07-01 20:54:59 +0200

    wip

--- server/web/src/routes.ts
+++
@@ -1,6 +0,0 @@
-import { type RouteConfig, route } from "@react-router/dev/routes";
-
-export default [
-  route("/", "./pages/Home.jsx"),
-  // pattern ^           ^ module file
-] satisfies RouteConfig;
--- web/src/components/login-form.tsx
+++ web/src/components/login-form.tsx
@@ -1,12 +1,15 @@
-import { cn } from "~/lib/utils"
-import { Button } from "~/components/ui/button"
+import { FormEvent, useCallback, useState } from "react";
+import { cn } from "~/lib/utils";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import { Button } from "~/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "~/components/ui/card"
+} from "~/components/ui/card";
 import logo from "~/assets/logo.svg";
 import { signIn } from "@hono/auth-js/react";
 
@@ -14,36 +17,43 @@ export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const onSignIn = useCallback((e: FormEvent<HTMLFormElement>) => {});
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader className="text-center">
-          <img src={logo} alt="logo" className="max-w-24 pb-5 mx-auto" />
+          <img src={logo} alt="logo" className="max-w-24 pb-2 mx-auto" />
           <CardTitle className="text-xl">Welcome back</CardTitle>
-          <CardDescription>
-            Login with your Google account
-          </CardDescription>
+          <CardDescription>Login with your credentials</CardDescription>
         </CardHeader>
         <CardContent>
-          <div className="grid gap-6">
-            <div className="flex flex-col gap-4">
-              <Button variant="outline" className="w-full" onClick={() => signIn('google')}>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                  <path
-                    d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                    fill="currentColor"
-                  />
-                </svg>
-                Login with Google
-              </Button>
+          <form className={"grid items-start gap-4"} onSubmit={onSignIn}>
+            <div className="grid gap-3">
+              <Input
+                type="text"
+                id="username"
+                name="username"
+                placeholder="Email or username"
+                required
+              />
+              <Input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="password"
+                required
+              />
+              <Button type="submit">Sign In</Button>
             </div>
-          </div>
+          </form>
         </CardContent>
       </Card>
       <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-        By clicking continue, you agree to our <a href="https://keenchat.com/terms">Terms of Service</a>{" "}
-        and <a href="https://keenchat.com/privacy">Privacy Policy</a>.
+        By clicking continue, you agree to our{" "}
+        <a href="https://keenchat.com/terms">Terms of Service</a> and{" "}
+        <a href="https://keenchat.com/privacy">Privacy Policy</a>.
       </div>
-    </div >
-  )
+    </div>
+  );
 }
