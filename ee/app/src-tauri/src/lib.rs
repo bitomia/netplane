fc970e3db6aff96ad03544908e0e1a9a2fa0abc9
Alejandro Real Ramírez <117752925+Alex64105@users.noreply.github.com>
2026-02-05 08:03:47 +0100

    feat: tray icon indicating connection state (#27)

--- ee/app/src-tauri/src/lib.rs
+++ ee/app/src-tauri/src/lib.rs
@@ -1,12 +1,13 @@
-use anyhow::anyhow;
 use dotenv::dotenv;
 use log::info;
 use netplane_client::client;
+use std::path::Path;
 use std::sync::Mutex;
 use tauri::{
+    image::Image,
     menu::{Menu, MenuItem},
     tray::TrayIconBuilder,
-    Manager,
+    Emitter, Manager,
 };
 use tokio_util::sync::CancellationToken;
 
@@ -15,32 +16,41 @@ struct AppState {
 }
 
 #[tauri::command]
-async fn client(
-    app_state: tauri::State<'_, AppState>,
-    server: &str,
-    auth: &str,
-    transport: &str,
-) -> tauri::Result<()> {
+async fn client(app_handle: tauri::AppHandle, server: String, auth: String, transport: String) {
+    app_handle
+        .emit("connecting", ())
+        .expect("start connecting emit error");
+
+    let app_state = app_handle.state::<AppState>();
+
     dotenv().ok();
 
     if server.is_empty() {
-        return Err(anyhow!("ERROR: No hay servidor").into())
+        app_handle
+            .emit("connect_error", "No server".to_string())
+            .expect("no server emit error");
+        log::error!("no server emit error");
+        return;
     }
 
     if let Err(err) = netplane_common::crypto::try_generate_crypto_keys("public.key", "private.key")
     {
         if err.kind() != std::io::ErrorKind::AlreadyExists {
-            return Err(err.into());
+            app_handle
+                .emit("connect_error", err.to_string())
+                .expect("alreadyExists crypto_keys emit error");
+            log::error!("alreadyExists crypto_keys emit error");
+            return;
         }
     }
 
     let host = server.to_string();
     let mut transport_type: Option<String> = None;
-    let mut tun_dev = "tun0".to_string();
-    let mut port: Option<u16> = Some(5000);
-    let mut auth_port: Option<u16> = Some(8000);
-    let mut loopback_relay = false;
-    let mut no_encryption = false;
+    let tun_dev = "tun0".to_string();
+    let port: Option<u16> = Some(5000);
+    let auth_port: Option<u16> = Some(8000);
+    let loopback_relay = false;
+    let no_encryption = false;
 
     if !transport.is_empty() {
         transport_type = Some(transport.to_string());
@@ -49,21 +59,34 @@ async fn client(
     if !auth.is_empty() {
         let link_code = auth;
 
-        client::auth_client(
+        if let Err(err) = client::auth_client(
             "auth.key",
             "public.key",
             "private.key",
             &host,
-            link_code,
+            &link_code,
             auth_port,
         )
-        .await?;
+        .await
+        {
+            app_handle
+                .emit("connect_error", err.to_string())
+                .expect("auth emit error");
+            log::error!("auth emit error");
+            return;
+        }
     }
 
     let cloned_token = {
         let mut token = match app_state.disconnect_token.lock() {
             Ok(t) => t,
-            Err(e) => return Err(anyhow!("{}", e).into()),
+            Err(err) => {
+                app_handle
+                    .emit("connect_error", err.to_string())
+                    .expect("cloned_token emit error");
+                log::error!("cloned_token emit error");
+                return;
+            }
         };
 
         if (*token).is_cancelled() {
@@ -73,7 +96,7 @@ async fn client(
         (*token).clone()
     };
 
-    client::run(
+    if let Err(err) = client::run(
         tun_dev,
         host,
         port,
@@ -82,16 +105,185 @@ async fn client(
         no_encryption,
         Some(cloned_token),
     )
-    .await?;
+    .await
+    {
+        app_handle
+            .emit("connect_error", err.to_string())
+            .expect("run emit error");
+        log::error!("run emit error");
+        return;
+    }
 
-    Ok(())
+    if let Some(tray) = app_handle.tray_by_id("main-tray") {
+        let connected_icon = match Image::from_path(Path::new("icons/connected/connected.png")) {
+            Ok(image) => image,
+            Err(err) => {
+                app_handle
+                    .emit("connect_error", err.to_string())
+                    .expect("connected_icon emit error");
+                log::error!("connected_icon emit error");
+                return;
+            }
+        };
+
+        if let Err(err) = tray.set_icon(Some(connected_icon)) {
+            app_handle
+                .emit("connect_error", err.to_string())
+                .expect("set_icon connect emit error");
+            log::error!("set_icon connect emit error");
+            return;
+        };
+
+        let show_item = match MenuItem::with_id(&app_handle, "show", "Show", true, None::<&str>) {
+            Ok(menu_item) => menu_item,
+            Err(err) => {
+                app_handle
+                    .emit("connect_error", err.to_string())
+                    .expect("show_item connect emit error");
+                log::error!("show_item connect emit error");
+                return;
+            }
+        };
+
+        let quit_item = match MenuItem::with_id(&app_handle, "quit", "Quit", true, None::<&str>) {
+            Ok(menu_item) => menu_item,
+            Err(err) => {
+                app_handle
+                    .emit("connect_error", err.to_string())
+                    .expect("quit_item connect emit error");
+                log::error!("quit_item connect emit error");
+                return;
+            }
+        };
+
+        let disconnect_item =
+            match MenuItem::with_id(&app_handle, "disconnect", "Disconnect", true, None::<&str>) {
+                Ok(menu_item) => menu_item,
+                Err(err) => {
+                    app_handle
+                        .emit("connect_error", err.to_string())
+                        .expect("disconnect_item connect emit error");
+                    log::error!("disconnect_item connect emit error");
+                    return;
+                }
+            };
+
+        let menu = match Menu::with_items(&app_handle, &[&show_item, &quit_item, &disconnect_item])
+        {
+            Ok(menu) => menu,
+            Err(err) => {
+                app_handle
+                    .emit("connect_error", err.to_string())
+                    .expect("menu connect emit error");
+                log::error!("menu connect emit error");
+                return;
+            }
+        };
+
+        if let Err(err) = tray.set_menu(Some(menu)) {
+            app_handle
+                .emit("disconnect_error", err.to_string())
+                .expect("set_menu connect emit error");
+            log::error!("set_menu connect emit error");
+            return;
+        }
+    }
+
+    info!("Connecting to dashboard");
+    app_handle
+        .emit("connected", ())
+        .expect("finish connecting emit error");
 }
 
 #[tauri::command]
-async fn stop_update(token_state: tauri::State<'_, AppState>) -> tauri::Result<()> {
-    token_state.disconnect_token.lock().unwrap().cancel();
+async fn stop_update(app_handle: tauri::AppHandle) {
+    app_handle
+        .emit("disconnecting", ())
+        .expect("start disconnecting emit error");
+    disconnect(app_handle).await
+}
 
-    Ok(())
+async fn disconnect(app_handle: tauri::AppHandle) {
+    let app_state = app_handle.state::<AppState>();
+
+    match app_state.disconnect_token.lock() {
+        Ok(lock) => lock.cancel(),
+        Err(err) => {
+            app_handle
+                .emit("disconnect_error", err.to_string())
+                .expect("disconnect_token emit error");
+            log::error!("disconnect_token emit error");
+            return;
+        }
+    };
+
+    if let Some(tray) = app_handle.tray_by_id("main-tray") {
+        let disconnected_icon =
+            match Image::from_path(Path::new("icons/disconnected/disconnected.ico")) {
+                Ok(image) => image,
+                Err(err) => {
+                    app_handle
+                        .emit("disconnect_error", err.to_string())
+                        .expect("disconnected_icon emit error");
+                    log::error!("disconnected_icon emit error");
+                    return;
+                }
+            };
+
+        if let Err(err) = tray.set_icon(Some(disconnected_icon)) {
+            app_handle
+                .emit("disconnect_error", err.to_string())
+                .expect("set_icon disconnect emit error");
+            log::error!("set_icon disconnect emit error");
+            return;
+        }
+
+        let show_item = match MenuItem::with_id(&app_handle, "show", "Show", true, None::<&str>) {
+            Ok(menu_item) => menu_item,
+            Err(err) => {
+                app_handle
+                    .emit("disconnect_error", err.to_string())
+                    .expect("show_item disconnect emit error");
+                log::error!("show_item disconnect emit error");
+                return;
+            }
+        };
+
+        let quit_item = match MenuItem::with_id(&app_handle, "quit", "Quit", true, None::<&str>) {
+            Ok(menu_item) => menu_item,
+            Err(err) => {
+                app_handle
+                    .emit("disconnect_error", err.to_string())
+                    .expect("quit_item disconnect emit error");
+                log::error!("quit_item disconnect emit error");
+                return;
+            }
+        };
+
+        let menu = match Menu::with_items(&app_handle, &[&show_item, &quit_item]) {
+            Ok(menu) => menu,
+            Err(err) => {
+                app_handle
+                    .emit("disconnect_error", err.to_string())
+                    .expect("menu disconnect emit error");
+                log::error!("menu disconnect emit error");
+                return;
+            }
+        };
+
+        if let Err(err) = tray.set_menu(Some(menu)) {
+            app_handle
+                .emit("disconnect_error", err.to_string())
+                .expect("set_menu disconnect emit error");
+            log::error!("set_menu disconnect emit error");
+            return;
+        }
+    }
+
+    info!("Disconnecting from dashboard");
+    app_handle
+        .emit("disconnected", ())
+        .expect("finish disconnecting emit error");
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -106,12 +298,16 @@ pub fn run() {
         })
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
-            let show = MenuItem::with_id(app, "show", "Show", true, None::<&str>)?;
-            let quit = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
-            let menu = Menu::with_items(app, &[&show, &quit])?;
+            let show_item = MenuItem::with_id(app, "show", "Show", true, None::<&str>)?;
+            let quit_item = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+
+            let menu = Menu::with_items(app, &[&show_item, &quit_item])?;
+
+            let disconnected_icon =
+                Image::from_path(Path::new("icons/disconnected/disconnected.ico"))?;
 
             TrayIconBuilder::with_id("main-tray")
-                .icon(app.default_window_icon().unwrap().clone())
+                .icon(disconnected_icon)
                 .menu(&menu)
                 .show_menu_on_left_click(false)
                 .on_menu_event(|app, event| match event.id.as_ref() {
@@ -123,6 +319,12 @@ pub fn run() {
                     }
                     "quit" => {
                         app.exit(0);
+                    }
+                    "disconnect" => {
+                        let app_handle = app.clone();
+                        tauri::async_runtime::spawn(async move {
+                            disconnect(app_handle).await;
+                        });
                     }
                     _ => {}
                 })
--- ee/app/src/pages/success.jsx
+++ ee/app/src/pages/success.jsx
@@ -1,14 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 import { invoke } from "@tauri-apps/api/core";
-import { useNavigate } from "react-router-dom";
+import { listen } from "@tauri-apps/api/event";
 
 export function Success( { isLogged, setIsLogged } ) {
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [disconnectMsg, setDisconnectMsg] = useState("Disconnect");
+    const [disableButton, setDisableButton] = useState(false);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const disconnecting_unlistener = listen('disconnecting', () => {
+            setErrorMsg(null);
+            setDisableButton(true);
+            setDisconnectMsg("Disconnecting...");
+            console.log("Disconnecting");
+        });
+
+        const disconnected_unlistener = listen('disconnected', () => {
+            setIsLogged(false);
+            navigate("/");
+            console.log("Disconnected");
+        });
+
+        const disconnect_error_unlistener = listen('disconnect_error', (error) => {
+            setErrorMsg(error.payload);
+            console.error("Couldn't disconnect: ", error.payload);
+            setDisconnectMsg("Disconnect");
+            setDisableButton(false);
+        });
+
+        return () => {
+            disconnected_unlistener.then(cleanup => cleanup());
+            disconnect_error_unlistener.then(cleanup => cleanup());
+            disconnecting_unlistener.then(cleanup => cleanup());
+        };
+    }, [navigate]);
     
     async function stop_update() {
         await invoke("stop_update", {});
-        setIsLogged(false);
-        navigate("/");
     }
     
     if(!isLogged) {
@@ -23,6 +53,7 @@ export function Success( { isLogged, setIsLogged } ) {
         ">
             <button
             type="submit"
+            disabled = {disableButton}
             className="
                 w-1/3 rounded-lg px-4 py-8 text-base font-medium
                 bg-neutral-800 text-white shadow-md
@@ -32,8 +63,14 @@ export function Success( { isLogged, setIsLogged } ) {
                 outline-none"
             onClick={stop_update}
             >
-            Disconnect
+            {disconnectMsg}
             </button>
+            {/* Error Message */}
+            {errorMsg && (
+                <p className="mt-6 text-base sm:text-lg text-center px-4 max-w-md text-red-600">
+                {errorMsg}
+                </p>
+            )}
         </main>
     )
 }
