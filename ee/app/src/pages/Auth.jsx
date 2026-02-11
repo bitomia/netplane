61cd377f87904ee1824bea9611284c7f4bf3ffc3
Alejandro Real Ramírez <117752925+Alex64105@users.noreply.github.com>
2026-02-11 13:21:26 +0100

    refactor: app structure more idiomatic (#28)
    Components have been created to improve reusability. Typescript has been added to improve error message appearance.

---
+++ ee/app/src/pages/Auth.jsx
@@ -0,0 +1,104 @@
+import "@radix-ui/themes/styles.css";
+import { useEffect, useState } from "react";
+import { invoke } from "@tauri-apps/api/core";
+import { useNavigate } from "react-router-dom";
+import { listen } from "@tauri-apps/api/event";
+
+import ErrorAlert from "../components/ErrorAlert.jsx";
+import LinkInput from "../components/LinkInput.jsx";
+import Logo from "../components/Logo.jsx";
+import Title from "../components/Title.jsx";
+import ToggleTransport from "../components/ToggleTransport.jsx";
+
+export function Auth({ setIsLogged }) {
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [server, setServer] = useState("");
+  const [auth, setAuth] = useState("");
+  const [transport, setTransport] = useState("");
+  const [startMsg, setStartMsg] = useState("Start now");
+  const [disableButton, setDisableButton] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const connectingUnlistener = listen('connecting', () => {
+      setErrorMsg(null);
+      setDisableButton(true);
+      setStartMsg("Starting...");
+      console.log("Starting");
+    }); 
+
+    const connectedUnlistener = listen('connected', () => {
+      setIsLogged(true);
+      navigate("/success");
+      console.log("Authed")
+    });
+
+    const connectErrorUnlistener = listen('connect_error', (error) => {
+      setErrorMsg(error.payload);
+      console.error("Couldn't create client: ", error);
+      setStartMsg("Start now");
+      setDisableButton(false);
+    });
+
+    return () => {
+      connectingUnlistener.then(cleanup => cleanup());
+      connectedUnlistener.then(cleanup => cleanup());
+      connectErrorUnlistener.then(cleanup => cleanup());
+    };
+  }, [navigate]);
+
+  async function client() {
+    await invoke("client", { server, auth, transport });
+  }
+
+  return (
+    <main
+      className="
+      min-h-screen flex flex-col items-center justify-center
+      px-4 sm:px-6 py-8 bg-neutral-100 dark:bg-neutral-900
+      text-neutral-900 dark:text-neutral-100"
+    >
+      <Logo />
+      <Title>
+        Software Defined Network
+      </Title>
+
+      {/* Form Section */}
+      <form
+        className="w-full max-w-xl flex flex-col gap-7 sm:gap-7 justify-center items-stretch sm:items-center px-4"
+        onSubmit={(e) => {
+          e.preventDefault();
+          client();
+        }}
+      >
+        <LinkInput
+          id="server-input"
+          onChange={(s) => setServer(s.currentTarget.value)}
+          placeholder="Enter server name..."
+        />
+        <LinkInput
+          id="link-input"
+          onChange={(a) => setAuth(a.currentTarget.value)}
+          placeholder="Enter auth code..."
+        />
+        <ToggleTransport onValueChange={setTransport} />
+
+        <button
+          type="submit"
+          disabled = {disableButton}
+          className="
+            w-1/3 mx-auto rounded-lg px-4 py-3 text-base font-medium
+            bg-neutral-800 text-white shadow-md
+            transition-all duration-250 cursor-pointer
+            hover:border-blue-600 active:border-blue-600 active:bg-neutral-200
+            dark:active:bg-neutral-700 dark:hover:border-white dark:active:border-white
+            outline-none"
+        >
+          {startMsg}
+        </button>
+      </form>
+
+      {errorMsg && <ErrorAlert>{errorMsg}</ErrorAlert>}
+    </main>
+  );
+}
